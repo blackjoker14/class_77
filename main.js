7ffbcadd0a928853dc974c6c91481912f2eccb21
blackjoker14 <86548199+blackjoker14@users.noreply.github.com>
2021-09-17 19:55:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var first_name = "Annan_Prasad";
+console.log(first_name);
+
+var last_name = "_Ghanto";
+console.log(last_name);
+
+first_name.concat(last_name);
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var length_of_full_name = full_name.length;
+console.log(length_of_full_name);
+
+var capital_name = "ANNAN";
+var making_lower_case = capital_name.toLowerCase();
+console.log(making_lower_case);
+
+var square_root = Math.sqrt(4);
+console.log(square_root);
+
+var maximum_number = Math.max(15, 10, 220, 2030);
+console.log(maximum_number);
+
+var minimum_number = Math.min(15, 10);
+console.log(minimum_number);
+
+var power_eg_1 = Math.pow(4, 2);
+console.log(power_eg_1);
